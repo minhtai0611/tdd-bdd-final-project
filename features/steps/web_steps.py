9b277b3f691bdce36c05944cd4de241883b946bb
minhtai0611 <minhtainth06111999@gmail.com>
2024-07-26 07:41:30 -0400

    commit

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,7 +104,10 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
 
 ##################################################################
 # This code works because of the following naming convention:
@@ -132,11 +135,6 @@ def step_impl(context, element_name, text_string):
     )
     element.clear()
     element.send_keys(text_string)
-
-@when('I press the "{button}" button')
-def step_impl(context, button):
-    button_id = button.lower() + '-btn'
-    context.driver.find_element_by_id(button_id).click()
 
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
